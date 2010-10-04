0b8712160c640a6cca0676e37151b7b657abf718
Jared Rummler <jrummler16@gmail.com>
2010-10-03 17:40:51 -0700

    pushing the rest of the files

---
+++ Android.mk
@@ -0,0 +1,54 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+#########################################################
+# First we install the script
+#
+
+LOCAL_MODULE := droidbox
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+#########################################################
+# Now we make our symlinks
+#
+
+SCRIPTYBOX_LINKS := $(shell cat $(LOCAL_PATH)/droidbox.links)
+
+# used to exclude anything
+exclude := 
+
+SYMLINKS := $(addprefix $(TARGET_OUT_OPTIONAL_EXECUTABLES)/,$(filter-out $(exclude),$(notdir $(SCRIPTYBOX_LINKS))))
+$(SYMLINKS): DROIDBOX_SCRIPT := $(LOCAL_MODULE)
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Symlink: $@ -> $(DROIDBOX_SCRIPT)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf $(DROIDBOX_SCRIPT) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
+
+# We need this so that the installed files could be picked up based on the
+# local module name
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
+    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
+
+#########################################################
+# Now we make copy our config files
+#
+
+etc_files := \
+	hosts.adblock \
+	hosts.local
+
+copy_to := $(addprefix $(TARGET_OUT)/etc/$(LOCAL_MODULE)/,$(etc_files))
+copy_from := $(addprefix $(LOCAL_PATH)/etc/,$(etc_files))
+
+$(copy_to) : PRIVATE_MODULE := system_etcdir
+$(copy_to) : $(TARGET_OUT)/etc/$(LOCAL_MODULE)/% : $(LOCAL_PATH)/etc/% | $(ACP)
+	$(transform-prebuilt-to-target)
+
+ALL_PREBUILT += $(copy_to)
